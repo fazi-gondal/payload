0a3a53cb40a4826605a623bd1e07d98e85d2c91a
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2025-08-26 17:47:27 +0500

    Update rootEditor.ts

--- src/fields/rootEditor.ts
+++ src/fields/rootEditor.ts
@@ -1,8 +1,6 @@
 // src/fields/rootEditor.ts
 import { lexicalEditor } from '@payloadcms/richtext-lexical';
-import type { LexicalEditorProps } from '@payloadcms/richtext-lexical';
 
-// This single line enables **all** lexical features
 export const rootEditor = lexicalEditor({
   features: ({ rootFeatures }) => rootFeatures,
-}) as LexicalEditorProps;
+});
