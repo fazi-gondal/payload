01734cfac9b257bc3129d20333ba3fcbc694c178
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2025-08-26 17:35:18 +0500

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -14,7 +14,7 @@ import { Users } from './collections/Users'
 import { Footer } from './Footer/config'
 import { Header } from './Header/config'
 import { plugins } from './plugins'
-import { defaultLexical } from '@/fields/defaultLexical'
+import { rootEditor } from '@/fields/rootEditor' // new file we create below
 import { getServerSideURL } from './utilities/getURL'
 
 const filename = fileURLToPath(import.meta.url)
@@ -58,7 +58,7 @@ export default buildConfig({
     },
   },
   // This config helps us configure global or default features that the other editors can inherit
-  editor: defaultLexical,
+  editor: rootEditor,
   db: vercelPostgresAdapter({
     pool: {
       connectionString: process.env.POSTGRES_URL || '',
